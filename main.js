97a9f22ba001994637e9e1c499da8a2e77c4b0b3
Madalena Machado Rocha <rochamada1997m@gmail.com>
2022-11-04 14:02:05 -0300

    class 4

--- main.js
+++ main.js
@@ -8,9 +8,11 @@ function createGame(player1, hour, player2) {
   `
 }
 
+let delay = -0.4
 function createCard(date, day, games) {
+  delay = delay + 0.4
   return `
-    <div class="card">
+    <div class="card" style="animation-delay: ${delay}s">
       <h2>${date} <span>${day}</span></h2>
       <ul>
         ${games}
@@ -19,18 +21,12 @@ function createCard(date, day, games) {
   `
 }
 
-document.querySelector("#app").innerHTML = `
-  <header>
-    <img src="./assets/logo.svg" alt="Logo da NLW" />
-  </header>
-  <main id="cards">
-    ${createCard("24/11", "quinta", createGame("brazil", "16:00", "serbia"))}
-    ${createCard(
-      "28/11",
-      "segunda",
-      createGame("switzerland", "13:00", "brazil") +
+document.querySelector("#cards").innerHTML =
+  createCard("24/11", "quinta", createGame("brazil", "16:00", "serbia")) +
+  createCard(
+    "28/11",
+    "segunda",
+    createGame("switzerland", "13:00", "brazil") +
       createGame("portugal", "16:00", "uruguai")
-    )}
-    ${createCard("02/12", "sexta", createGame("brazil", "16:00", "cameroon"))}
-  </main>
-`
+  ) +
+  createCard("02/12", "sexta", createGame("brazil", "16:00", "cameroon"))
